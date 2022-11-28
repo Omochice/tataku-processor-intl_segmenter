83b49309375b7d10e4ebd182a5189ac0c8269738
Omochice <mochice.mls.ntl@gmail.com>
2022-11-28 22:38:09 +0900

    :muscle: apply interface

---
+++ denops/@tataku/processor/intl_segmenter.ts
@@ -0,0 +1,36 @@
+import { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
+import {
+  isObject,
+  isString,
+} from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+import { Processor } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
+
+export default class implements Processor {
+  constructor(private readonly option: Record<string, undefined>) {
+    if (!isOption(option)) {
+      throw new Error(
+        `Option must have "locale" as string: ${JSON.stringify(option)}`,
+      );
+    }
+  }
+
+  async run(
+    _denops: Denops,
+    source: string[],
+  ): Promise<string[]> {
+    const segmenter = new Intl.Segmenter(this.option.locale, {
+      granularity: this.option.granularity ?? "sentence",
+    });
+
+    return Array.from(segmenter.segment(source.join(""))).map((s) => s.segment);
+  }
+}
+
+type Option = {
+  granularity?: "grapheme" | "word" | "sentence";
+  locale: string;
+};
+
+function isOption(x): x is Option {
+  return isObject(x, isString) && isString(x.locale);
+}
